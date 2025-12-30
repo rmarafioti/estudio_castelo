09a882e07564b7d5d9dd00d9ea3bdb42a45cc396
Rich Marafioti <richmarafioti@gmail.com>
2025-12-30 15:07:58 -0600

    #9 adds tattoing section header

--- src/app/_data/photos.js
+++ src/app/_data/photos.js
@@ -1,9 +1,9 @@
 export const heroPhoto = {
   alt: "...",
   desktop: {
-    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1766876282/estudio_castelo/hero_section_ptceg3.png",
-    height: 936,
-    width: 1249,
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1767126905/estudio_castelo/hero_section_desktop_ermcvi.png",
+    height: 893,
+    width: 1205,
   },
   mobile: {
     src: "https://res.cloudinary.com/dzpne110u/image/upload/v1766964450/estudio_castelo/hero_section_mobile_qru1zj.png",
@@ -23,5 +23,19 @@ export const reviewSectionBackground = {
     src: "https://res.cloudinary.com/dzpne110u/image/upload/v1767057669/estudio_castelo/reviews_section_bg_mobile_qduzje.png",
     height: 1029,
     width: 392,
+  },
+};
+
+export const tattooHeaderBackground = {
+  alt: "...",
+  desktop: {
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1767127040/estudio_castelo/tattoo_page_header_bg_sdho4y.png",
+    height: 823,
+    width: 1440,
+  },
+  mobile: {
+    src: "https://res.cloudinary.com/dzpne110u/image/upload/v1767127461/estudio_castelo/tattoo_page_header_bg_mobile_mhcomb.png",
+    height: 793,
+    width: 393,
   },
 };
--- src/app/tattooing/page.js
+++ src/app/tattooing/page.js
@@ -1,17 +1,34 @@
 "use client";
 
 import FadeInSection from "../_components/Fade_In_Section";
+import Responsive_Image from "../_components/Responsive_image";
+import { tattooHeaderBackground } from "../_data/photos";
+
+import styles from "../_styling/tattooing.module.css";
 
 export default function Tattooing() {
   return (
     <main>
-      <FadeInSection>
-        <h1>Tattooing</h1>
-        <h2>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet
-          consectetur adipiscing elit quisque faucibus.
-        </h2>
-      </FadeInSection>
+      <article className={styles.tattooing_header_section}>
+        <div className={styles.tattooing_image_container}>
+          <Responsive_Image photoData={tattooHeaderBackground} />
+        </div>
+        <FadeInSection className={styles.tattooing_copy_container}>
+          <h1 className={styles.header}>Tattooing</h1>
+          <p className={styles.copy}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            feugiat pulvinar nisl sed viverra. Proin dapibus sed metus sit amet
+            rhoncus. Nam ante felis, tempus vitae leo vel, interdum ultrices
+            ipsum. Nullam tincidunt orci massa, et commodo lectus vulputate
+            quis. Maecenas eu suscipit diam. Cras porta eros eros, in vehicula
+            ipsum dignissim porta. Vestibulum imperdiet massa ut augue blandit,
+            in vestibulum nisl mollis. Ut justo massa, ultrices ullamcorper
+            imperdiet bibendum, dapibus sit amet urna. Sed eleifend velit vel
+            malesuada cursus. Etiam dapibus nunc lacus, et dignissim augue
+            interdum at.
+          </p>
+        </FadeInSection>
+      </article>
     </main>
   );
 }
