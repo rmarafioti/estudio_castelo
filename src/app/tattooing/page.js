6cd90b519c3043360ca184e76ad758f0bcea7ce3
Rich Marafioti <richmarafioti@gmail.com>
2026-01-10 10:37:45 -0600

    feat(Gallery): add reusable photo gallery component
    - Create reusable photo gallery component
    - Import and integrate gallery on tattooing page
    - Add desktop styling for tattooing page gallery
    
    Refs: #11

--- src/app/tattooing/page.js
+++ src/app/tattooing/page.js
@@ -2,11 +2,17 @@
 
 import FadeInSection from "../_components/Fade_In_Section";
 import Responsive_Image from "../_components/Responsive_image";
+import usePhotoGallery from "../_hooks/usePhotoGallery";
+import Gallery from "../_components/Gallery";
 import { tattooHeaderBackground } from "../_data/photos";
+import { tattooPhotos } from "../_data/tattooGallery";
 
 import styles from "../_styling/tattooing.module.css";
 
 export default function Tattooing() {
+  const { handleNext, handlePrev, currentImageObj, currentIndex, photos } =
+    usePhotoGallery(tattooPhotos);
+
   return (
     <main>
       <article className={styles.tattooing_header_section}>
@@ -24,6 +30,15 @@ export default function Tattooing() {
           </p>
         </FadeInSection>
       </article>
+      <FadeInSection className={styles.tattoo_gallery}>
+        <Gallery
+          onNext={handleNext}
+          onPrev={handlePrev}
+          currentIndex={currentIndex}
+          currentImageObj={currentImageObj}
+          photos={photos}
+        />
+      </FadeInSection>
     </main>
   );
 }
